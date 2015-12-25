78aeda897b9cc2da9a8fe35f76c707998d58dad2
liangjian <liangjian@oliveche.com>
2015-12-25 14:21:58 +0800

    release gzip版本

--- build_it.sh
+++ build_it.sh
@@ -6,4 +6,5 @@ VIM=gvim
 target=vimwiki-lj-$VER
 cd src
 $VIM -c "let g:vimball_home='.' | %MkVimball! $target" -c "qa!" filelist.txt
-echo "=== generate src/$target"
+gzip ${target}.vmb
+echo "=== generate src/${target}.vmb.gz"
